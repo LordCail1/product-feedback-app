75f0e30b7a0a2dbe2f47fa69ffc5c56849854909
LordCail1 <rami.batnigi@gmail.com>
2023-08-07 11:42:53 -0400

    finished roadmap page

--- src/app/home/@optionsMenu/@roadmap/page.tsx
+++ src/app/home/@optionsMenu/@roadmap/page.tsx
@@ -2,26 +2,37 @@ import Link from "next/link"
 
 export default function Page() {
 	return (
-		<div className="relative mb-6 flex rounded-xl bg-white p-6 flex-col">
-			<div className="flex justify-between">
-				<h3>Roadmap</h3>
-				<Link href={"/roadmap"} className="">View</Link>
+		<div className="relative mb-6 flex flex-col rounded-xl bg-white p-6">
+			<div className="mb-6 flex justify-between items-center">
+				<h3 className="font-bold text-lg">Roadmap</h3>
+				<Link
+					href="/roadmap"
+					className="font-semibold text-rainbow_fish underline transition-colors hover:text-periwinkle_blue text-base"
+				>
+					View
+				</Link>
 			</div>
 			<ul className="flex flex-col">
-				<li>
-					<div></div>
-					<span></span>
-					<span></span>
+				<li className="mb-4 flex justify-between">
+					<div className="flex items-center">
+						<div className="h-2 w-2 rounded-full bg-creamy_peach"></div>
+						<span className="ml-4">Planned</span>
+					</div>
+					<span className="font-bold">6</span>
 				</li>
-				<li>
-					<div></div>
-					<span></span>
-					<span></span>
+				<li className="mb-4 flex justify-between">
+					<div className="flex items-center">
+						<div className="h-2 w-2 rounded-full bg-singapore_orchid"></div>
+						<span className="ml-4">In-Progress</span>
+					</div>
+					<span className="font-bold">6</span>
 				</li>
-				<li>
-					<div></div>
-					<span></span>
-					<span></span>
+				<li className="mb-4 flex justify-between">
+					<div className="flex items-center">
+						<div className="h-2 w-2 rounded-full bg-blue_mana"></div>
+						<span className="ml-4">Live</span>
+					</div>
+					<span className="font-bold">6</span>
 				</li>
 			</ul>
 		</div>
--- src/assets/colors.ts
+++ src/assets/colors.ts
@@ -1,16 +1,17 @@
 const colors = {
-    singapore_orchid: "#AD1FEA",
-    rainbow_fish: "#4661E6",
-    raven_night: "#373F68",
-    garden_fairy: "#CDD2EE",
+    blue_mana: "#62BCFA",
     cotton_ball: "#F2F4FF",
+    creamy_peach: "#F49F85",
+    garden_fairy: "#CDD2EE",
     ghost_white: "#F7F8FD",
     jewel_cave: "#3A4374",
     ocean_night: "#647196",
-    creamy_peach: "#F49F85",
-    blue_mana: "#62BCFA",
     pale_phthalo_blue: "#CFD7FF",
-    white: "#ffff"
+    periwinkle_blue: "#8397F8",
+    rainbow_fish: "#4661E6",
+    raven_night: "#373F68",
+    singapore_orchid: "#AD1FEA",
+    white: "#ffff",
 }
 
 export default colors
